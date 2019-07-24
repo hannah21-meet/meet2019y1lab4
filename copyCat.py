f462b39f32df1cf7b528513db571637a2d70b607
hannah21-meet <hannah21@meet.mit.edu>
2019-07-24 03:24:59 -0400

    Stopping here

---
+++ copyCat.py
@@ -0,0 +1,14 @@
+import turtle
+greg=turtle.Turtle()
+ahmad=turtle.Turtle()
+greg.shape('arrow')
+greg.color('green')
+greg.pensize(5)
+greg.pencolor('green')
+greg.goto(0,-150)
+greg.goto(75,-150)
+greg.color('blue')
+greg.pencolor('blue')
+greg.goto(75,0)
+greg.left(90)
+
---
+++ funTurtle.py
@@ -0,0 +1,18 @@
+import turtle
+turtle.bgcolor('yellow')
+turtle.shape('circle')
+bob=turtle.clone()
+bob.shape('square')
+bob.goto(100,0)
+bob.goto(100,100)
+bob.goto(0,100)
+bob.goto(0,0)
+turtle.bgcolor('black')
+charles=turtle.Turtle()
+charles.pensize(20)
+charles.pencolor('green')
+charles.goto(100,100)
+
+
+
+
